71ef9f127a708a549ad729d53191467267d0f815
kelvinarrudasilva <kelvinesx@gmail.com>
2025-11-13 16:13:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-client_id = "90dede77-a858-442d-ba05-39e37c1694ac"
-client_secret = "ce76d0f2-ee35-49d1-84a4-6e4fb906e0c7"
-authorization_code = "NOVO_CODE_AQUI"
-
 import requests
 
-url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
+url = "https://login.microsoftonline.com/consumers/oauth2/v2.0/token"
 
 data = {
-    "client_id": client_id,
+    "client_id": "90dede77-a858-442d-ba05-39e37c1694ac",
     "scope": "Files.Read offline_access",
-    "code": authorization_code,
+    "code": "M.C509_SN1.2.U.a6120e1e-5296-7715-14ce-6d6e265e6033",  # seu code
     "redirect_uri": "http://localhost:8501",
     "grant_type": "authorization_code",
-    "client_secret": client_secret
+    "client_secret": "ce76d0f2-ee35-49d1-84a4-6e4fb906e0c7"
 }
 
 res = requests.post(url, data=data)
+print(res.status_code)
 print(res.json())
